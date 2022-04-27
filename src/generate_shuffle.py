0dd2390221c46c87590989a521091e23e07f3208
Vilém Zouhar <vilem.zouhar@gmail.com>
2022-04-27 11:18:59 +0200

    baked queue generation

---
+++ src/generate_shuffle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from utils import load_data, load_data_structure, save_json
+import random
+import csv
+
+
+data = load_data_structure()
+
+UIDs = [
+    "harare", "lusaka", "sahara", "cardiff", "hanoi",
+    "caracas", "montevideo", "washington", "kampala", "funafuti",
+    "ashgabat", "ankara", "tiraspol", "lome", "bangkok",
+    "dodoma", "dushanbe", "damascus", "bern", "stockholm",
+    "paramaribo", "khartoum", "madrid", "juba", "seoul",
+    "pretoria", "hargeisa", "mogadishu", "honiara", "ljubljana",
+    "bratislava", "philipsburg", "singapore", "freetown", "belgrade",
+]
+
+for uid_i, uid in enumerate(UIDs[:3]):
+    random.seed(uid_i)
+    new_data = []
+
+    keys = list(data.keys())
+    random.shuffle(keys)
+    data_shuffled = [l for k in keys for l in data[k]]
+
+    mapping = {"systems": [], "docs": keys, "doc_len": [len(data[k]) for k in keys]}
+
+    for sent in data_shuffled:
+        order = [1,2,3,4]
+        random.shuffle(order)
+        
+        mapping["systems"].append(order)
+        new_sent = []
+        for i in order:
+            new_sent.append(sent[i])
+        new_data.append(new_sent)
+
+    with open(f"data/translations_{uid}.csv", "w") as f:
+        csv.writer(f, quoting=csv.QUOTE_ALL).writerows(new_data)
+
+    save_json(f"data/mapping_{uid}.json", mapping)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,43 @@
 import csv
 import pickle
+import json
 import numpy as np
 
-def load_data(filename="translations.csv"):
+def load_data(filename="data/translations.csv"):
     with open(filename, "r") as f:
         data = list(csv.reader(f))[1:]
     # filter by second column
     data = [line[:6] for line in data if len(line[1]) != 0]
     return data
+
+def load_data_structure(filename="data/translations.csv"):
+    with open(filename, "r") as f:
+        # strip header
+        data = list(csv.reader(f))[1:]
+    
+    data_new = {}
+    bucket = []
+    cur_doc = None
+
+    for line in data:
+        if len(line[0]) != 0 and all([len(line[i]) == 0 for i in range(1, 3)]):
+            # new document
+            cur_doc = line[0]
+        elif all([len(line[i]) == 0 for i in range(0,len(line))]) and len(bucket) != 0:
+            if cur_doc is None:
+                raise Exception()
+            assert cur_doc not in data_new
+            data_new[cur_doc] = bucket
+            cur_doc = None
+            bucket = []
+        elif len(line[1]) != 0:
+            # proper text
+            bucket.append(line[:6])
+    
+    if cur_doc is not None:
+        data_new[cur_doc] = bucket
+
+    return data_new
 
 
 def get_device():
@@ -27,3 +57,12 @@ def save_pickle(path, data):
     with open(path, "wb") as fwrite:
         pickler = pickle.Pickler(fwrite)
         pickler.dump(data)
+
+
+def read_json(path):
+    with open(path, "r") as fread:
+        return json.load(fread)
+
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
