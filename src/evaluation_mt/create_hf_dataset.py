b1e3b92fb5237d7e194c6d4663a5b2719731d8e4
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-03-03 17:59:10 +0100

    add references and WMT data

--- src/evaluation_mt/create_hf_dataset.py
+++ src/evaluation_mt/create_hf_dataset.py
@@ -1,17 +1,36 @@
 import sys
 sys.path.append("src")
-import utils
-import collections
 import json
+import collections
+import utils
+
+REF_TYPE = ["R1", "R2", "R3", "R4"]
 
 data_wmt = utils.load_wmt()
 data_out = collections.defaultdict(lambda: {"src": None, "systems": {}})
 
+
+data_human_raw = utils.load_json("data/ort_human.json")
+data_human = collections.defaultdict(dict)
+for doc in data_human_raw:
+    for line in doc["lines"]:
+        for ref_type, ref_type_val in line["translations"].items():
+            ref_type = REF_TYPE[int(ref_type)-1]
+            data_human[line["source"]][ref_type] = ref_type_val["orig"]
+            data_human[line["source"]][
+                ref_type + "_pe_" + doc["expertise"]+"_" + doc["uid"]
+            ] = ref_type_val["done"]
+
 for line in data_wmt:
     data_out[line["src"]]["src"] = line["src"]
-    data_out[line["src"]]["systems"][line["system"]] = { "tgt": line["tgt"], "score": line["score"] }
+    data_out[line["src"]]["systems"][line["system"]] = {
+        "tgt": line["tgt"], "score": line["score"]
+    }
 
 data_out = list(data_out.values())
+
+for line in data_out:
+    line["ref"] = data_human[line["src"]]
 
 with open("data/ort_wmt.json", "w") as f:
     json.dump(data_out, f, ensure_ascii=False, indent=2)
